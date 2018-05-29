df9ae15b907cb9a224e5c3b26c6e296a4ef4793c
FractalFox <flgtom@aol.com>
2018-05-29 01:32:52 -0700

    development stage: Adding script *for create.ejs

---
+++ public/scripts/create.js
@@ -0,0 +1,52 @@
+let number = document.getElementById('number');
+let filename = document.getElementById('filename');
+let submit = document.getElementById('submit');
+let goback = document.getElementById('back');
+let choicenames = document.getElementById('choicenames');
+
+console.log("working");
+
+goback.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/";
+});
+
+number.addEventListener('keyup',function(){
+  console.log("number focusout");
+  console.log(!choicenames.hasChildNodes());
+  if(!choicenames.hasChildNodes()){
+    for(var t = 0; t < number.value; t++){
+      let node = document.createElement("INPUT");
+      node.setAttribute("type","text");
+      node.setAttribute("name","choice[choice" + (t+1) + "]");
+      node.setAttribute("style","width:100px;height:50px;");
+      node.setAttribute("required", "true");
+      choicenames.appendChild(node);
+    }
+  }
+
+});
+
+filename.addEventListener('keypress', function(e){
+  let tmp = e.which;
+  if(tmp === 32){
+
+    return false;
+  }
+});
+filename.addEventListener('focusout', function(){
+  console.log("focusout");
+  let count = 0;
+  let check = filename.value;
+  for(var k = 0; k < check.length; k++){
+    if(check[k] == " "){
+      count++;
+    }
+  }
+  if(count != 0){
+    submit.disabled = true;
+    submit.style['background-color']  = "#f00";
+  }else{
+    submit.disabled = false;
+    submit.style['background-color'] = "#eee";
+  }
+});
