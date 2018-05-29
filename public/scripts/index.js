4a84c00915c0e5f6f471b4aecbe70df4495e083a
FractalFox <flgtom@aol.com>
2018-05-29 14:43:20 -0700

    development stage: script updating *for index.ejs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,13 @@
+let create = document.getElementById('create');
+let search = document.getElementById('search');
+
+create.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/create";
+});
+search.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/search";
+});
+
 let github = document.getElementById('github');
 let email = document.getElementById('email');
 
