b8cb406d1734dff167e13184b848db7ecf0a461e
FractalFox <flgtom@aol.com>
2018-06-01 16:16:57 -0700

    development stage: simple voting working. no auth.

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(session({
     }),
     secret: process.env.BUM,
     cookie: {
-            expires: 1209600000 // 14 days
-        }
+            expires: 15 * 60 * 1000 // 15 min
+        },
+    resave: false,
+    saveUninitialized: false
 }));
 
 const port = process.argv[2] || 3000;
@@ -24,6 +26,39 @@ const excluded = ["index.ejs","login.ejs","search.ejs","forbidden.ejs","create.e
 
 const dir = "views/";
 
+function searchAll(){
+  let fileArr = [];
+  let count = 0;
+  fs.readdir(dir, (err, data) => {
+    if(err) {
+      let tried = "Directory Read Error";
+      tried += "<p>" + err.stack + "</p>";
+      return tried;
+    }
+
+    let len = data.length;
+    for(var i = 0;i < len; i++){
+      for(var j = 0; j < excluded.length; j++){
+        // if found file is also in excluded.
+        if(data[i].toString() == excluded[j]){
+          break;
+        }else{
+          count++;
+        }
+      }
+      if(count == excluded.length){
+        let tmp = data[i].toString();
+        tmp = tmp.substring(0, tmp.length - 4 );
+        fileArr.push(tmp);
+        count = 0;
+      }else{
+        count = 0;
+      }
+    }
+  });
+  return fileArr;
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(1000+max));
 }
@@ -31,14 +66,16 @@ function getRandomInt(max) {
 // The function buildpage is used for dynamically creating ejs pages and, for updating a single pre-defined value within.
 // Arguments are: buildpage(String, String, Integer, Array[String], String)
 // Respectively: name of file to create, description of file, Number of names, array of names, the ID of the element to increment.
-function buildPage(filename, desc, num, names, update){
+function buildPage(filename, desc, num, names, update, colors){
   if(filename[0] == "."){
     filename = filename.substring(1);
   }
+  let funFlag = false;
   console.log("Filename: " + filename);
   console.log("Description: " + desc);
   console.log("Number of Choices: " + num);
-  if(update != undefined){
+  if(update !== undefined ){
+    funFlag = true;
     console.log("Updating vote: " + update);
   }
   let newpage = "<!DOCTYPE html>";
@@ -74,16 +111,21 @@ function buildPage(filename, desc, num, names, update){
   newpage += "<nav id='nav'><div id='back' class='nav-btn'>Create New</div><div id='home' class='nav-btn'>Home</div></nav><hr/>";
   newpage += "<button id='reload' type='button'>Change Colors</button>";
   newpage += "<section id='title'>";
-  newpage += "<h4>post name: " + filename + "</h4>";
-  newpage += "<h4>"+desc+"</h4>";
+  newpage += "<h4 id='filename' value='"+filename+"'>post name: " + filename + "</h4>";
+  newpage += "<h4 id='desc'>"+desc+"</h4>";
   newpage += "</section>";
   newpage += "<div id='wrapper'>";
   for(var j = 0;j < num; j++){
-    let marble = getRandomInt(j+1);
-    newpage += "<div class='obj" + j + " option'><h3 style='text-align:center;'>" + names[j] + "</h3>";
-    newpage += "<div id='vote"+j+"' value='0' style='width:auto;height:80%;color:#fff;background-color:#" + (((marble <= 999) && (marble >= 101)) ? marble : ("00" + j) ) +  ";'>0";
-
-    newpage += "</div></div>";
+    if(funFlag){
+      newpage += "<div class='obj" + j + " option'><h3 style='text-align:center;'>" + names[j] + "</h3>";
+      newpage += "<div id='vote"+j+"' style='width:auto;height:80%;color:#fff;background-color:" + colors[j] +  ";'>" + update[j];
+      newpage += "</div></div>";
+    }else{
+      let marble = getRandomInt(j+1);
+      newpage += "<div class='obj" + j + " option'><h3 style='text-align:center;'>" + names[j] + "</h3>";
+      newpage += "<div id='vote"+j+"' style='width:auto;height:80%;color:#fff;background-color:#" + (((marble <= 999) && (marble >= 101)) ? marble : ("00" + j) ) +  ";'>0";
+      newpage += "</div></div>";
+    }
   }
   newpage += "</div>";
   newpage += "</body>";
@@ -189,7 +231,7 @@ var bodyParser = require('body-parser'); app.use(bodyParser.json()); app.use(bod
 //=================================================== CREATE ========================================================================
 // {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
 //   {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
-// {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{ BEING DELETED {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
+// {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
 //   {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
 // ================================================== CREATE ========================================================================
 // The form needs to be re-done;
@@ -266,38 +308,13 @@ app.get('/search', function(req, res, next){
 
   // /search uses the fs module to return the directory contents of views/
   // This function uses the array "excluded" to check for files that should NOT be displayed to the user.
-  let fileArr = [];
-  let count = 0;
-  fs.readdir(dir, (err, data) => {
-    if(err) {
-      let tried = "Directory Read Error";
-      tried += "<p>" + err.stack + "</p>";
-      res.render('404.ejs',{ tried });
-    }
+  let fileArr = searchAll();
 
-    let len = data.length;
-    for(var i = 0;i < len; i++){
-      for(var j = 0; j < excluded.length; j++){
-        if(data[i].toString() == excluded[j]){
-          break;
-        }else{
-          count++;
-        }
-      }
-      if(count == excluded.length){
-        let tmp = data[i].toString();
-        tmp = tmp.substring(0, tmp.length - 4 );
-        fileArr.push(tmp);
-        count = 0;
-      }else{
-        count = 0;
-      }
-    }
-  });
   setTimeout(function(){
     console.log(fileArr);
     res.render('search.ejs', { fileArr });
   },1000);
+
 
 });
 
@@ -310,20 +327,26 @@ app.get('/search', function(req, res, next){
 app.get('/lookup:qwry', function(req, res, next){
 
   let flag = false;
+  let tmp = "";
   //console.log(req.params.qwry);
-  let tmp = req.params.qwry;
-  tmp = tmp.substring(1);
+  if(req.params.qwry){
+    tmp = req.params.qwry;
+    tmp = tmp.substring(1);
+  }else{
+    tmp = "void";
+  }
 
   for(var i = 0; i < excluded.length; i++){
     //console.log(tmp == excluded[i].toString())
-    if(tmp == excluded[i].toString()){
+    if(excluded[i].toString().includes(tmp)){
       flag = true;
     }
     //console.log('next');
   }
   if(flag){
     let tried = tmp;
-    res.render('404.ejs',{ tried });
+    let status = res.statusCode;
+    res.render('404.ejs',{ tried, status });
   }else{
     if(req.session[tmp+"_views"]){
       req.session[tmp+"_views"]++;
@@ -346,26 +369,156 @@ app.get('/lookup:qwry', function(req, res, next){
 //   {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
 // ============================================== /Test =========================================================================
 app.post('/test', function(req, res, next){
-
   // bird is ID of vote element, skunk is page title.
   // currently working. still needs to update ejs document with vote. done here.
   // check if IP has made vote on page. 1 vote per IP?. mongodb for ip's or file list?
-  let skunk = req.body['title'].toString().split('<')[1].toString().split(':')[1].toString().substring(1) + ".ejs";
-  console.log("Vote for: " + req.body.bird + "\nIn: " + skunk);
-  //res.clearCookie("page=Testing.ejsFri%20Jun%2001…Z%2F6hxjh5i28lJEwUgkN2F4iBjwo");
-  //res.cookie("page",skunk + new Date());
+  console.log(req.body);
+  let desc = req.body.desc;
+
+  let names = req.body.names;
+  names = names.split(',');
+  names.pop();
+
+  let num = names.length;
+
+  let skunk = req.body['title'].toString();
+  //console.log("Vote for: " + req.body.bird + "\nIn: " + skunk);
+
+  console.log("creation values:");
+  console.log(skunk + "\n" + desc + "\n" + num + "\n" + names + "\n" + req.body.bird);
+
   if(req.session[skunk+"_votes"]){
-      //req.session[skunk+"_views"]++;
       res.send();
-      //res.send("You visited this page " + req.session.page_views + " times");
-   } else {
-      req.session[skunk+"_votes"] = 1;
-      res.send(req.body.bird);
-      //res.send("Welcome to this page for the first time!");
-   }
+  } else {
+    req.session[skunk+"_votes"] = 1;
+//     let postbody = buildPage(skunk, desc, num, names, req.body.bird);
+
+//     fs.writeFile("views/" + filename + ".ejs", postbody, function(err){
+//       if(err) {
+//         console.log(err.stack);
+//         next();
+//       }
+//       console.log("new file created");
+//     });
+
+    res.send(req.body.bird);
+  }
+});
+
+//===============================================================================================================>
+//==============================================Functions========================================================>
+//===============================================================================================================>
+function cb(req, res, temp){
+  // will do nothing, cb to be removed.
+  // should add GET to /fetch in user.js at appropriate point.
+  // user.js already handles update, I just need to change it in the fs for future viewers.
+  res.status(200).set({'content-type':'text/html'});
+  res.write(temp);
+  res.end()
+}
+
+function changepage(req, res, next, file, vote, cb){
+  fs.readFile("views/"+file+".ejs", function(err,data){
+    if(err){
+      console.log(err.stack);
+    }
+    if(data){
+      // vtArr is an array of integers. represents current total votes for each index respecively.
+      let vtArr = [];
+      let names = [];
+      let colors = [];
+      let num = 0;
+      let desc = data.toString().split("id='desc'")[1].toString().split("</h4>")[0].toString().substring(1);
+      // num will equal names.length
+      let arf = data.toString().split('section')[2].split('option');
+      // arf[1].toString() example
+      // Requesting URL: /fetch?file=testing.ejs&vote=vote1
+      // '><h3 style='text-align:center;'>One</h3><div id='vote0' style='width:auto;height:80%;color:#fff;background-color:#183;'>0</div></div><div class='obj1
+      for(var i = 1; i < arf.length; i++){
+        let tmp = arf[i].split('</div>')[0].toString();
+        let name = tmp.substring(33, tmp.split('</h3>')[0].length);
+        let color = tmp.split('background-color:')[1].substring(0, 4);
+
+        //console.log("color:",color);
+        colors.push(color);
+        names.push(name);
+        // tmp = '><h3 style='text-align:center;'>One</h3><div id='vote0' style='width:auto;height:80%;color:#fff;background-color:#183;'>0
+        //console.log(tmp);
+        if(("vote"+i) == vote){
+          vtArr.push((Number(tmp[tmp.length-1])+1));
+          //console.log("id vote" + (i-1) + " is now " + (Number(tmp[tmp.length-1])+1));
+
+        }else{
+          vtArr.push(tmp[tmp.length-1]);
+          //console.log("id vote" + (i-1) + " is still " + tmp[tmp.length-1]);
+        }
+      }
+      // buildPage(filename, desc, num, names, update, colors)
+      // use buildpage()? to build temp, then erase old and rename temp as old.
+      console.log(file);// filename
+      console.log(desc);// description
+      console.log(num = names.length);// num of vote items
+      console.log(names.toString());// name of each vote item
+      console.log(vtArr.toString());// array of vote totals
+      let tempPage = buildPage(file, desc, num, names, vtArr, colors);
+
+      fs.writeFile("views/" + file +".ejs", tempPage, function(err){
+      if(err) {
+        console.log(err.stack);
+        next();
+      }
+      console.log("new file created");
+    });
+      // cb will be deleted after done codeing here.
+      // just so i can see in a browser that nothing failed.
+      //cb(req, res, tempPage);
+    }else{
+      next();
+    }
+  })
+
+}
+//=====================fetch test============================
+//
+//=====================fetch test============================
+app.get('/fetch', function(req, res, next){
+  // if(req.session[+"_votes"]){
+  //     res.send();
+  // } else {
+  //   req.session[+"_votes"] = 1;
+  // }
 
 
-})
+  // Does not return any page. just makes server-side changes.
+  // tests if request contains a file and vote query.
+
+  // loop in changepage is dependent on starting at one.
+  // incoming vote needs to take into consideration.
+  // ie. vote0 will do absolutely nothing.
+  let flag = true;
+  if(req.query.file && req.query.vote){
+    if(req.session[req.query.name+"_votes"]){
+      req.session[req.query.name+"_votes"]++;
+    }else{
+      req.session[req.query.name+"_votes"] = 1;
+    }
+    for(var i = 0; i < excluded.length; i++){
+      if(req.query.file == excluded[i].toString()){
+        flag = false;
+      }
+    }
+    if(flag){
+      let file = req.query.file;
+      let vote = req.query.vote;
+      changepage(req, res, next, file, vote, cb);
+    }else{
+      res.status(404);
+      next();
+    }
+  }else{
+    next();
+  }
+});
 
 //=============================================== 403 & 404 ERRORs ==================================================================
 // {{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}{{}}
@@ -397,15 +550,16 @@ app.all('*', function(req, res, next){
     // triggers on all other unknown requests to the server.
     // Treating as a not found error ( 404 ).
 
-    console.log("some other error");
-    console.log(req.body);
-    console.log(res.statusCode);
+    console.log("Error");
+    console.log("POST params " + req.body);
+    //console.log(res.statusCode);
     // res.status(404);
     // res.write("<h1>Status Code " + res.statusCode + "</h1>");
     // res.write("<h1>Failed to find " + req.originalUrl + "</h1>");
     // res.end();
     let tried = req.originalUrl;
-    res.render('404.ejs',{tried});
+    let status = res.statusCode;
+    res.render('404.ejs',{tried, status});
   }
 
 });
