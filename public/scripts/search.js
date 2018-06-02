8174ac72dd56167ca0beeb977554330f4668b3db
FractalFox <flgtom@aol.com>
2018-06-01 21:47:55 -0700

    development stage: buttons added.

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,7 +1,12 @@
-let goback = document.getElementById('back');
+let create = document.getElementById('create');
+let home = document.getElementById('home');
 
 console.log("working");
 
-goback.addEventListener('click', function(){
+create.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/create";
+});
+
+home.addEventListener('click', function(){
   window.location = "https://voteit.glitch.me/";
 });
