f6ac8a1af3c1e10ff7ed57740e82ebdb8b29e472
FractalFox <flgtom@aol.com>
2018-06-01 21:48:46 -0700

    development stage: buttons added. mobile css added

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,57 @@
-let create = document.getElementById('create');
+let number = document.getElementById('number');
+let filename = document.getElementById('filename');
+let submit = document.getElementById('submit');
+let home = document.getElementById('home');
 let search = document.getElementById('search');
+let choicenames = document.getElementById('choicenames');
 
-create.addEventListener('click', function(){
-  window.location = "https://voteit.glitch.me/create";
+console.log("working");
+
+home.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/";
 });
+
 search.addEventListener('click', function(){
   window.location = "https://voteit.glitch.me/search";
 });
 
-let github = document.getElementById('github');
-let email = document.getElementById('email');
+number.addEventListener('keyup',function(){
+  console.log("number focusout");
+  console.log(!choicenames.hasChildNodes());
+  if(!choicenames.hasChildNodes()){
+    for(var t = 0; t < number.value; t++){
+      let node = document.createElement("INPUT");
+      node.setAttribute("type","text");
+      node.setAttribute("name","choice[choice" + (t+1) + "]");
+      node.setAttribute("style","width:100px;height:50px;");
+      node.setAttribute("required", "true");
+      choicenames.appendChild(node);
+    }
+  }
 
-github.addEventListener('click', function(){
-  if(email.checked){
-    email.checked = false;
+});
+
+filename.addEventListener('keypress', function(e){
+  let tmp = e.which;
+  if(tmp === 32){
+
+    return false;
   }
 });
-email.addEventListener('click', function(){
-  if(github.checked){
-    github.checked = false;
+filename.addEventListener('focusout', function(){
+  console.log("focusout");
+  let count = 0;
+  let check = filename.value;
+  for(var k = 0; k < check.length; k++){
+    if(check[k] == " "){
+      count++;
+    }
+  }
+  if(count != 0){
+    submit.disabled = true;
+    submit.style['background-color']  = "#f00";
+  }else{
+    submit.disabled = false;
+    submit.style['background-color'] = "#eee";
   }
 });
