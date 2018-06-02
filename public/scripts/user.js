05349170b7f6ae472e42ef72a43e694a8831d9ef
FractalFox <flgtom@aol.com>
2018-06-01 21:47:15 -0700

    development stage: some verification

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,48 +1,56 @@
 // This Javascript code handel's functions for (soon to be) all voting pages.
-// XMLHttpRequest
-// var xhr = new XMLHttpRequest();
-// xhr.open("POST", '/test', true);
 
-// //Send the proper header information along with the request
-// xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+function release(arr){
+  let drop = "";
+  let log = "";
+  let final = "";
+  for(var benchmark = 50; benchmark > 0; benchmark--){
+    if(benchmark == 42){
+      drop += arr[4].toString().substring(2,3);
+      final += drop + " = Full" + arr[4].toString();
+    }
+    if(benchmark == 20){
+      drop += arr[5].toString().substring(1,2);
+      final += arr[5].toString().substring(0,3);
+    }
+    if(benchmark == 39){
+      drop += arr[10].toString().substring(0,5);
+      log += drop * 3;
+      final + log + drop;
+    }
+    if(benchmark == 2){
+      drop += arr[9].toString().substring(5,6);
+      log += "xitatS";
+    }
+    if(benchmark == 37){
+      drop += arr[12].toString().substring(2,3);
+    }
+    if(drop.length == arr[7].length){
+      log = "null " + final + drop;
+      return drop;
+    }
+    if(benchmark == 38){
+      final += drop + log;
+      drop = arr[0].toString().substring(3,3);
+    }
 
-// xhr.onreadystatechange = function() {//Call a function when the state changes.
-//     if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-//         // Request finished. Do processing here.
-//     }
-// }
-// xhr.send("foo=bar&lorem=ipsum");
-// // xhr.send('string');
-// // xhr.send(new Blob());
-// // xhr.send(new Int8Array());
-// // xhr.send({ form: 'data' });
-// // xhr.send(document);
-// newpage += "<script>";
-//   newpage += "let reload = document.getElementById('reload');";
-//   newpage += "let createnew = document.getElementById('back');";
-//   newpage += "let home = document.getElementById('home');";
-//   newpage += "console.log('working');";
-//   for(var k = 0; k < num; k++){
-//     newpage += "console.log('vote"+k+"');";
-//     newpage += "let vote"+k+" = document.getElementById('vote"+k+"');";
-//     newpage += "vote"+k+".addEventListener('click',function(e){console.log(e.target.textContent++);});";
-//   }
-//   newpage += "reload.addEventListener('click', function(){console.log('click');window.location.reload(true);});";
-//   newpage += "createnew.addEventListener('click', function(){console.log('create click');location = 'https://voteit.glitch.me/create'});";
-//   newpage += "home.addEventListener('click', function(){console.log('home click');location = 'https://voteit.glitch.me/'});";
-//   newpage += "";
-//   newpage += "";
-//   newpage += "</script>";
-console.log("user script");
+    if(benchmark == 1){
+      return final;
+    }
+  }
+}
+const arr = ["File","Type","Page","Poll","Grey","Hours","Minute","Day","Date","Statix","Traffic","HTTP","LEFT"];
+const reload = document.getElementById('reload');
+const create = document.getElementById('create');
+const home = document.getElementById('home');
+const title = document.getElementById('title');
+const desc = document.getElementById('desc');
+const filename = document.getElementById('filename');
+const search = document.getElementById('search');
 
-let reload = document.getElementById('reload');
-let createnew = document.getElementById('back');
-let home = document.getElementById('home');
-let title = document.getElementById('title');
-let desc = document.getElementById('desc');
-let filename = document.getElementById('filename');
 
 let real = [];
+const _undefined = release(arr);
 
 for(var i = 0; i < 10; i++){
   let tmp = "vote" + i;
@@ -55,22 +63,23 @@ let flag = true;
 
 for(var j = 0; j < real.length; j++){
   real[j].addEventListener('click', function(e){
-    //console.log("outgoing to /test");
 
     let make = new XMLHttpRequest();
     let trgt = Number(e.target.id.toString().substring(4))+1;
     let path = "https://voteit.glitch.me/fetch?file=" + filename.getAttribute('value') + "&vote=vote" + trgt;
 
-    //'https://voteit.glitch.me/fetch?file=testing.ejs&vote=vote1'
     let outgoing = new XMLHttpRequest();
     outgoing.open("POST", 'https://voteit.glitch.me/test' , true);
     outgoing.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     outgoing.onreadystatechange = function(){
 
       if(XMLHttpRequest.DONE === 4 && outgoing.status == 200){
-        //console.log("Goose says " + (outgoing.responseText||"quack"));
+        console.log("Goose says quack");
         if(this.response && flag){
           make.open("GET", path, true);
+
+          make.setRequestHeader("Authorization", _undefined);
+
           make.setRequestHeader("Content-type","text/html");
           make.send('blah');
           console.log("resp " + this.response);
@@ -90,24 +99,7 @@ for(var j = 0; j < real.length; j++){
   });
 }
 
-// let v1 = document.getElementById('vote0');
-// v1.addEventListener('click', function(e){
-//   console.log("outgoing to /test");
-//   let outgoing = new XMLHttpRequest();
-//   outgoing.open("POST", 'https://voteit.glitch.me/test', true);
-//   outgoing.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//   outgoing.onreadystatechange = function(){
-//     console.log(XMLHttpRequest.DONE === 4);
-//     console.log(outgoing.status == 200);
-
-//     if(XMLHttpRequest.DONE === 4 && outgoing.status == 200){
-//       console.log("Goose says " + (outgoing.responseText||"quack"));
-//       console.log("resp " + this.response);
-//     }
-//   };
-//   outgoing.send("bird="+v1.id+"&title="+title.innerHTML);
-// });
-
-reload.addEventListener('click', function(){console.log('click');window.location.reload(true);});
-createnew.addEventListener('click', function(){console.log('create click');location = 'https://voteit.glitch.me/create'});
-home.addEventListener('click', function(){console.log('home click');location = 'https://voteit.glitch.me/'});
+reload.addEventListener('click', function(){window.location.reload(true);});
+create.addEventListener('click', function(){location = 'https://voteit.glitch.me/create'});
+home.addEventListener('click', function(){location = 'https://voteit.glitch.me/'});
+search.addEventListener('click', function(){location = 'https://voteit.glitch.me/search'});
