d37c321a6bd2b0e58ed7d6c88daccab88d6bd5f7
FractalFox <flgtom@aol.com>
2018-06-04 09:57:26 -0700

    development stage: Updating script

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,57 +1,27 @@
-let number = document.getElementById('number');
-let filename = document.getElementById('filename');
-let submit = document.getElementById('submit');
-let home = document.getElementById('home');
+let create = document.getElementById('create');
 let search = document.getElementById('search');
-let choicenames = document.getElementById('choicenames');
+let about = document.getElementById('about');
 
-console.log("working");
-
-home.addEventListener('click', function(){
-  window.location = "https://voteit.glitch.me/";
+create.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/create";
 });
-
 search.addEventListener('click', function(){
   window.location = "https://voteit.glitch.me/search";
 });
-
-number.addEventListener('keyup',function(){
-  console.log("number focusout");
-  console.log(!choicenames.hasChildNodes());
-  if(!choicenames.hasChildNodes()){
-    for(var t = 0; t < number.value; t++){
-      let node = document.createElement("INPUT");
-      node.setAttribute("type","text");
-      node.setAttribute("name","choice[choice" + (t+1) + "]");
-      node.setAttribute("style","width:100px;height:50px;");
-      node.setAttribute("required", "true");
-      choicenames.appendChild(node);
-    }
-  }
-
+about.addEventListener('click', function(){
+  window.location = "https://voteit.glitch.me/about";
 });
 
-filename.addEventListener('keypress', function(e){
-  let tmp = e.which;
-  if(tmp === 32){
+let github = document.getElementById('github');
+let email = document.getElementById('email');
 
-    return false;
+github.addEventListener('click', function(){
+  if(email.checked){
+    email.checked = false;
   }
 });
-filename.addEventListener('focusout', function(){
-  console.log("focusout");
-  let count = 0;
-  let check = filename.value;
-  for(var k = 0; k < check.length; k++){
-    if(check[k] == " "){
-      count++;
-    }
-  }
-  if(count != 0){
-    submit.disabled = true;
-    submit.style['background-color']  = "#f00";
-  }else{
-    submit.disabled = false;
-    submit.style['background-color'] = "#eee";
+email.addEventListener('click', function(){
+  if(github.checked){
+    github.checked = false;
   }
 });
