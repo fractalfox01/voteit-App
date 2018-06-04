0c88b011621ab321b221f99291ff6c9c0b1ed6e7
FractalFox <flgtom@aol.com>
2018-06-04 09:56:27 -0700

    development stage: Updating script

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,5 +1,13 @@
 let create = document.getElementById('create');
 let home = document.getElementById('home');
+let foot = document.getElementById('myfooter');
+
+foot.addEventListener('mouseenter', function(){
+  foot.innerHTML = "Created By Thomas Vandivier";
+});
+foot.addEventListener('mouseout', function(){
+  foot.innerHTML = "Created By EncodedVoid";
+});
 
 console.log("working");
 
